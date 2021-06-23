e95bd0776cf0880b2906ee70edab25cf47b1ad57
Brandon Macdonald <bmacdonald1986@gmail.com>
2021-06-22 23:32:48 -0600

    Modify recipe card styling and layout

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Container, Grid } from '@material-ui/core';
+import {
+  Container, Grid, AppBar, Toolbar, Box, Typography,
+} from '@material-ui/core';
 import RecipeCard from '../recipe/RecipeCard';
 
 const Dashboard = () => (
-  <Container>
-    <Grid container>
-      <Grid item>
-        <RecipeCard />
+  <Box>
+    <AppBar position="static" color="secondary">
+      <Toolbar>
+        <Typography variant="h4">RECIME </Typography>
+      </Toolbar>
+    </AppBar>
+    <Container>
+      <Grid container>
+        <Grid item />
+        <Grid item>
+          <RecipeCard />
+        </Grid>
+        <Grid item>
+          <RecipeCard />
+        </Grid>
+        <Grid item>
+          <RecipeCard />
+        </Grid>
       </Grid>
-      <Grid item>
-        <RecipeCard />
-      </Grid>
-      <Grid item>
-        <RecipeCard />
-      </Grid>
-    </Grid>
-  </Container>
+    </Container>
+  </Box>
 );
 
 export default Dashboard;
--- src/features/recipe/RecipeCard.jsx
+++ src/features/recipe/RecipeCard.jsx
@@ -16,11 +16,11 @@ const RecipeCard = () => (
   <StyledPaper>
     <Grid container>
       <Grid item xs="12">
-        <Box display="flex" flexDirection="row" justifyContent="space-between">
+        <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
           <Box mb={1}>
             <Typography variant="h5">This is a recipe</Typography>
           </Box>
-          <Box display="flex" flexDirection="row" alignContent="center">
+          <Box display="flex" flexDirection="row" alignItems="center">
             <FavoriteIcon />
             <Typography variant="caption" align="center">12</Typography>
           </Box>
@@ -28,18 +28,21 @@ const RecipeCard = () => (
       </Grid>
       <Grid item><Typography variant="body1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus doloremque minima fugit natus esse temporibus magnam incidunt, ad facilis eos, atque suscipit repudiandae consectetur iste illo sequi ratione vel labore numquam repellendus voluptatibus sed. Facere, consequatur. Molestias magni vero iste error dolore. Modi facilis beatae officiis maiores non, eaque rem.</Typography></Grid>
       <Grid item xs="12">
-        <Box display="flex" flexDirection="row" alignContent="center" m={1} justifyContent="space-between">
-          <Box>
-            <IconButton size="small" color="secondary">
+        <Box display="flex" flexDirection="row" alignItems="center" m={1} justifyContent="space-between">
+          <Box display="flex" alignItems="center">
+            <IconButton size="small" color="primary">
               <ArrowUpwardIcon />
             </IconButton>
             <IconButton size="small" color="secondary">
               <ArrowDownwardIcon />
             </IconButton>
+            <Box ml={1}>
+              <Typography variant="body1">Vote!</Typography>
+            </Box>
           </Box>
           <Box>
             <Typography variant="body2">
-              comments(0)
+              comments (0)
             </Typography>
           </Box>
         </Box>
